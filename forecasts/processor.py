3903de4e5a58721292286963407caf410db2099c
Yiheng Li <liyiheng.scott@gmail.com>
2019-02-12 11:57:27 -0800

    🏗 [data] adjusted data structures

--- forecasts/processor.py
+++ forecasts/processor.py
@@ -1,9 +1,21 @@
 import os
 import pandas as pd
 import numpy as np
+from abc import ABC, abstractmethod
 
 
-class YahooProcessor:
+class Processor(ABC):
+
+    @abstractmethod
+    def get_returns(tickers, folder, freq, fromdate, todate, data_col):
+        pass
+
+    @abstractmethod
+    def get_all_tickers(folder):
+        pass
+
+
+class YahooProcessor(Processor):
     """
     The set of pre-processors to handle data downloaded from Yahoo APIs
     """
@@ -22,8 +34,7 @@ class YahooProcessor:
         return pd.read_csv(os.path.join(folder, ticker+'.csv'),
                            index_col='Date', parse_dates=True)
 
-    @staticmethod
-    def get_returns(ticker, folder=None, freq='d', fromdate='2000-01-01',
+    def _get_return(ticker, folder=None, freq='d', fromdate='2000-01-01',
                     todate='2018-12-31', data_col='Adj Close', is_log=False):
         df = YahooProcessor.load_data(ticker, folder)
         df = df.resample(freq).last()
@@ -33,3 +44,22 @@ class YahooProcessor:
             return df[data_col].pct_change()
         else:
             return np.log(1 + df[data_col].pct_change())
+
+    @staticmethod
+    def get_returns(tickers, folder=None, freq='d', fromdate='2000-01-01',
+                    todate='2018-12-31', data_col='Adj Close', is_log=False):
+        res = {}
+        for ticker in tickers:
+            res[ticker] = YahooProcessor._get_return(ticker, folder, freq,
+                                                     fromdate, todate,
+                                                     data_col, is_log)
+        return res
+
+    @staticmethod
+    def get_all_tickers(folder=None):
+        if folder is None:
+            folder = YahooProcessor.get_default_folder()
+
+        tickers = os.listdir(folder)
+
+        return [t[:-4] for t in tickers if t.endswith('.csv')]
