ea3e671bc8ffb73f0accbb307e3048f7b8e60f8f
Yiheng Li <liyiheng.scott@gmail.com>
2019-02-13 02:43:52 -0800

    ✨ [model] added rolling framework

--- forecasts/models.py
+++ forecasts/models.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from utils import get_logger, get_tqdm
+import numpy as np
 
 
 class ModelBase(ABC):
@@ -12,11 +14,50 @@ class ModelBase(ABC):
         pass
 
 
-class RollingRegressionModel(ModelBase):
+class RollingMethod:
 
-    def __init__(self, rolling_period=90, predict_period=1):
-        self.rolling_period = rolling_period
-        self.predict_period = 1
+    def __init__(self, rolling_bars=90, predict_bars=1, task_name='rolling',
+                 is_debug=False, train_args=None, predict_args=None):
+        self.rolling_bars = rolling_bars
+        self.predict_bars = predict_bars
+        self.is_debug = is_debug
+        self.task_name = task_name
 
-    def train(self, x, y):
-        pass
+        self.train_args = {} if train_args is None else train_args
+        self.predict_args = {} if predict_args is None else predict_args
+
+    def log(self, text):
+        if self.is_debug:
+            logger = get_logger()
+            logger.info("<{}> - {}".format(self.task_name, text))
+
+    def run(self, model, x, y):
+        if len(x) <= self.rolling_bars:
+            raise Exception("RollingMethod cannot be initialzed due to size "
+                            "is smaller than the rolling periods.")
+
+        arr = np.arange(len(x) - self.rolling_bars)
+        arr = arr[arr % self.predict_bars == 0] + self.rolling_bars
+        arr = np.concatenate([arr, [len(x)]])
+
+        predictions = []
+        tqdm, ascii = get_tqdm()
+        for train_end, predict_end in tqdm(zip(arr[:-1], arr[1:]),
+                                           ascii=ascii, total=len(arr)-1):
+            train_x = x[train_end - self.rolling_bars: train_end]
+            train_y = y[train_end - self.rolling_bars: train_end]
+
+            self.log("Training from bar %d to bar %d..." %
+                     (train_end - self.rolling_bars, train_end - 1))
+            model.train(train_x, train_y, **self.train_args)
+
+            predict_x = x[train_end: predict_end]
+            predict_y = y[train_end: predict_end]
+
+            self.log("Predicting from bar %d to bar %d..." %
+                     (train_end, predict_end - 1))
+            predictions.append(
+                model.predict(predict_x, predict_y, **self.predict_args))
+
+        return np.concatenate(
+            [np.repeat(np.nan, self.rolling_bars), predictions])
---
+++ forecasts/utils.py
@@ -0,0 +1,69 @@
+from tqdm import tqdm, tqdm_notebook
+import logging
+import colorlog
+import os
+
+
+def in_ipynb():
+    """
+    Check if the current environment is IPython Notebook
+
+    Note, Spyder terminal is also using ZMQShell but cannot render Widget.
+
+    Returns
+    -------
+    bool
+        True if the current env is Jupyter notebook
+    """
+    try:
+        zmq_status = str(type(get_ipython())) == "<class 'ipykernel.zmqshell.ZMQInteractiveShell'>" # noqa E501
+        spyder_status = any('SPYDER' in name for name in os.environ)
+        return zmq_status and not spyder_status
+
+    except NameError:
+        return False
+
+
+def get_tqdm():
+    """
+    Get proper tqdm progress bar instance based on if the current env is
+    Jupyter notebook
+
+    Note, Windows system doesn't supprot default progress bar effects
+
+    Returns
+    -------
+    type, bool
+        either tqdm or tqdm_notebook, the second arg will be ascii option
+    """
+    ascii = True if os.name == 'nt' else False
+
+    if in_ipynb():
+        # Jupyter notebook can always handle ascii correctly
+        return tqdm_notebook, False
+    else:
+        return tqdm, ascii
+
+
+class TqdmHandler(logging.StreamHandler):
+    def __init__(self):
+        colorlog.StreamHandler.__init__(self)
+
+    def emit(self, record):
+        msg = self.format(record)
+        tqdm.write(msg)
+
+
+def get_logger(name='afp'):
+    logger = colorlog.getLogger(name)
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    handler = TqdmHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            '%(log_color)s%(asctime)s - %(log_color)s %(levelname)-6s%(reset)s %(white)s%(message)s')) # noqa E501
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
