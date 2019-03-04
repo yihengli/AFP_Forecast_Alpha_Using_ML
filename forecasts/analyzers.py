82df6e1e15d2d90d13ff2528e13e707be1fe2b50
Yiheng Li <liyiheng.scott@gmail.com>
2019-03-04 02:39:32 -0800

    ⚡️ [analyzer] improved RMSE calculation and struct

--- forecasts/analyzers.py
+++ forecasts/analyzers.py
@@ -5,7 +5,8 @@ import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 import numpy as np
 import seaborn as sns
-from typing import Dict, Optional, List
+import os
+from typing import Dict, Optional, List, Tuple
 
 import matplotlib as mpl
 mpl.use('Agg')
@@ -60,7 +61,7 @@ class AnalyzerSingle:
 
     @staticmethod
     def get_rolling_metric(y_true, y_pred, metric='squared_error',
-                           name=None):
+                           name=None, rolling_bars=30):
 
         if metric == 'abs_error':
             errors = np.abs(y_true - y_pred)
@@ -70,11 +71,26 @@ class AnalyzerSingle:
             errors = (y_true - y_pred)**2
             errors.name = name
             return errors
+        elif metric == 'RMSE':
+            # Optimized RMSE calculation with vectorizations
+            df_pred = pd.DataFrame(y_pred)
+            df_pred.columns = [0]
+            df_true = pd.DataFrame(y_true)
+            df_true.columns = [0]
+            for i in range(rolling_bars-1):
+                df_pred[i+1] = df_pred[0].shift(i+1)
+                df_true[i+1] = df_true[0].shift(i+1)
+            df_pred = df_pred.dropna().values
+            df_true = df_true.dropna().values
+            df_error = (df_pred - df_true) ** 2
+            res = np.concatenate((np.repeat(np.nan, rolling_bars-1),
+                                  np.sqrt(df_error.mean(1))))
         else:
             func = REGRESSION_METRICS[metric]
-            res = [np.nan]
-            for i in range(1, len(y_true)):
-                res.append(func(y_true[:i], y_pred[:i]))
+            res = np.repeat(np.nan, len(y_true))
+            for i in range(rolling_bars, len(y_true)+1):
+                res[i-1] = (func(y_true[(i-rolling_bars):i],
+                                 y_pred[(i-rolling_bars):i]))
 
         if name is None:
             name = metric
@@ -90,7 +106,8 @@ class Analyzer:
 
     def analyze(self, pred_path, fromdate, todate, is_debug=False,
                 show_rolling=True, output_file=None,
-                rolling_metrics=['squared_error'], labels=None, **label_args):
+                rolling_metrics=['squared_error'],
+                rolling_bars=30, labels=None, **label_args):
         logger = get_logger()
         tqdm, ascii = get_tqdm()
         if labels is None:
@@ -133,12 +150,17 @@ class Analyzer:
 
             # Calculate Rolling Cumulative Errors
             if show_rolling:
-                for rolling_metric in rolling_metrics:
-                    rollings[rolling_metric].append(
-                        AnalyzerSingle.get_rolling_metric(
-                            trues, preds, rolling_metric, name=ticker
+                try:
+                    for rolling_metric in rolling_metrics:
+                        rollings[rolling_metric].append(
+                            AnalyzerSingle.get_rolling_metric(
+                                trues, preds, rolling_metric, name=ticker,
+                                rolling_bars=rolling_bars
+                            )
                         )
-                    )
+                except ValueError:
+                    logger.warning("Size Not Enough <%s>" % ticker)
+                    continue
 
         # Concatenate results as dataframes
         logger.info('Task Finished: %d Attempted and %d Failed.' %
@@ -173,13 +195,16 @@ class Plotter:
                  fromdate: str,
                  todate: str,
                  rolling_metrics: List[str] = ['squared_error'],
-                 label_args: Optional[Dict] = None) -> None:
+                 rolling_bars: int = 30,
+                 label_args: Optional[Dict] = None,
+                 stock_meta_path: Optional[str] = None) -> None:
 
         self.pred_path = pred_paths
         self.item_names = item_names
         self.fromdate = fromdate
         self.todate = todate
         self.rolling_metrics = rolling_metrics
+        self.rolling_bars = rolling_bars
 
         if label_args is None:
             label_args = {}
@@ -189,6 +214,16 @@ class Plotter:
 
         cmap = sns.color_palette()
         self.colors = [mpl.colors.rgb2hex(c[:3]) for c in cmap]
+
+        # stock_meta_path is a csv contains each stock's market cap and sector
+        if stock_meta_path is None:
+            self.meta = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), os.pardir,
+                             'yahoos', 'yahoo_metas.csv'))
+
+    def _load_meta(self):
+        if self.meta is not None:
+            self.meta_data = pd.read_csv(self.meta)
 
     @staticmethod
     def _exclude_outliers(df: pd.DataFrame, col: str) -> pd.DataFrame:
@@ -212,6 +247,9 @@ class Plotter:
         labels = get_labels(fromdate=self.fromdate, todate=self.todate,
                             is_debug=False, **self.label_args)
 
+        logger.info("Loading Meta Data at %s" % self.meta)
+        self._load_meta()
+
         for pred, name in zip(self.pred_path, self.item_names):
             logger.info("Analyzing %s..." % name)
 
@@ -220,9 +258,9 @@ class Plotter:
                 fromdate=self.fromdate,
                 todate=self.todate,
                 rolling_metrics=self.rolling_metrics,
+                rolling_bars=self.rolling_bars,
                 labels=labels
             )
-
             if exclude_outlier:
                 self.results[name]["Metrics"] = self._exclude_outliers(
                     self.results[name]["Metrics"], 'RMSE')
@@ -241,59 +279,91 @@ class Plotter:
                           for name in self.item_names]
         return res_df
 
-    def get_distribution_plot(self, metric: str = 'RMSE') -> mpl.figure.Figure:
-        rows = int(np.ceil(len(self.item_names) / 2))
+    @staticmethod
+    def plot_histogram(ax: mpl.axes._base._AxesBase, data: pd.DataFrame,
+                       color: str, name: str, metric: str,
+                       avg_color: str = 'w') -> mpl.axes._base._AxesBase:  # noqa E501
+        data.hist(ax=ax, bins=30, color=color)
+        ax.set_title("%s (%s)" % (name, metric), fontweight=700)
+        ax.axvline(data.mean(), color='red', ls=':',
+                   label="Average %s" % metric)
+        ax.annotate('%.5f' % data.mean(), xy=(data.mean(), 0),
+                    xytext=(2, 2), textcoords='offset points', color=avg_color)
+        ax.legend()
+
+    @staticmethod
+    def _plot_distribution(datasets: List[pd.DataFrame], names: List[str],
+                           colors: List[str], metric: str) -> mpl.figure.Figure:  # noqa E501
+
+        rows = int(np.ceil(len(names) / 2))
         fig, axes = plt.subplots(rows, 2, figsize=(14, rows * 5),
                                  sharex=True, sharey=True)
         axes = axes.flatten()
 
-        for ax, name, color in zip(axes, self.item_names, self.colors):
-            data = self.results[name]['Metrics'][metric]
-            data = data.loc[self.stocks]
+        for ax, data, name, color in zip(axes, datasets, names, colors):
+            Plotter.plot_histogram(ax, data, color, name, metric)
 
-            data.hist(ax=ax, bins=30, color=color)
-            ax.set_title("%s (%s)" % (name, metric), fontweight=700)
-            ax.axvline(data.mean(), color='red', ls=':',
-                       label="Average %s" % metric)
-            ax.annotate('%.5f' % data.mean(), xy=(data.mean(), 0),
-                        xytext=(2, 2), textcoords='offset points', color='w')
-            ax.legend()
-
-        if len(axes) > len(self.item_names):
+        if len(axes) > len(names):
             axes[-1].set_visible(False)
 
         return fig
 
-    def get_cumulative_metrics_plot(self,
-                                    metric: str = 'squared_error',
-                                    q1: float = 0.75,
-                                    q2: float = 0.25) -> mpl.figure.Figure:
+    @staticmethod
+    def plot_cum_lines(ax: mpl.axes._base._AxesBase,
+                       datasets: List[pd.DataFrame], names: List[str],
+                       colors: List[str], metric: str, rolling_bars: int,
+                       q: Optional[Tuple[float]]) -> mpl.figure.Figure:
 
-        fig, ax = plt.subplots(1, 1, figsize=(10, 4))
-        if metric not in REGRESSION_METRICS:
+        if metric in ['squared_error', 'abs_error']:
             condition = 'Cumulative'
         else:
-            condition = 'Rolling'
+            condition = 'Rolling (%d bars)' % rolling_bars
 
         ax.set_title('%s %s Over Time' % (condition, metric), fontweight=700)
 
-        for name, color in zip(self.item_names, self.colors):
-            rmean = self.results[name]['Rolling'][metric][self.stocks]\
-                 .quantile(.5, 1)
-            rup = self.results[name]['Rolling'][metric][self.stocks]\
-                .quantile(q1, 1)
-            rdown = self.results[name]['Rolling'][metric][self.stocks]\
-                .quantile(q2, 1)
+        for data, name, color in zip(datasets, names, colors):
+            rmean = data.quantile(.5, 1)
 
-            if metric not in REGRESSION_METRICS:
+            if q is not None:
+                q1, q2 = q
+                rup = data.quantile(q1, 1)
+                rdown = data.quantile(q2, 1)
+
+            if metric in ['squared_error', 'abs_error']:
                 ax.plot(rmean.cumsum(), color=color, label=name + ' Median')
-                ax.plot(rup.cumsum(), color=color, 
-                        label=name + ' {:.0%} Percentile'.format(q1), ls='-.')
-                ax.plot(rdown.cumsum(), color=color,
-                        label=name + ' {:.0%} Percentile'.format(q2), ls=':')
+                if q is not None:
+                    ax.plot(rup.cumsum(), color=color, ls='-.', alpha=0.33,
+                            label=name + ' {:.0%} Percentile'.format(q1))
+                    ax.plot(rdown.cumsum(), color=color, ls=':', alpha=0.33,
+                            label=name + ' {:.0%} Percentile'.format(q2))
             else:
-                ax.plot(rmean, color=color, label=name)
+                ax.plot(rmean, color=color, label=name + ' Median')
+                if q is not None:
+                    ax.plot(rup, color=color, ls='-.', alpha=0.33,
+                            label=name + ' {:.0%} Percentile'.format(q1))
+                    ax.plot(rdown, color=color, ls=':', alpha=0.33,
+                            label=name + ' {:.0%} Percentile'.format(q2))
 
         ax.legend()
 
+    def get_distribution_plot(self, metric: str = 'RMSE') -> mpl.figure.Figure:
+
+        datasets = [self.results[name]['Metrics'][metric].loc[self.stocks]
+                    for name in self.item_names]
+        return Plotter._plot_distribution(datasets, self.item_names,
+                                          self.colors, metric)
+
+    def get_cumulative_metrics_plot(self,
+                                    metric: str = 'squared_error',
+                                    q: Optional[Tuple[float]] = (.25, .75)) -> mpl.figure.Figure:  # noqa 501
+
+        datasets = []
+        for name in self.item_names:
+            df = self.results[name]['Rolling'][metric]
+            datasets.append(df[df.columns[df.columns.isin(self.stocks)]])
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+
+        Plotter.plot_cum_lines(ax, datasets, self.item_names,
+                               self.colors, metric, self.rolling_bars, q)
         return fig
